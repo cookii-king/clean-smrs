cbdfe5a4fc4412d67f20e003276f9813162394e6
Hassan-Naveed1 <hassann4242@gmail.com>
2024-12-12 04:42:58 +0000

    Fixed Links

--- django/accounts/urls.py
+++ django/accounts/urls.py
@@ -8,7 +8,7 @@ from .views import HomePageView
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login'),
-    path('account', AccountView.as_view(), name='account'),
+    path('account/', AccountView.as_view(), name='account'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('', HomePageView.as_view(), name='home'),  # Correct usage of HomePageView as a class-based view
 ]
