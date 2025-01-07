83ab3274fc20ed67ccb320c9f4cb5814c29118e5
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2025-01-07 09:57:18 +0000

    testing an easy server setup.bash file

--- utilities/server/setup.bash
+++ utilities/server/setup.bash
@@ -39,3 +39,45 @@ python3 manage.py create_superuser_if_none
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+# step 7
+# Define the source and destination paths
+gunicorn_utilities_conf_setup="utilities/django/gunicorn.conf"
+gunicorn_server_conf_setup="/etc/supervisor/conf.d/gunicorn.conf"
+echo "Copying configuration files from utilities..."
+sudo cp "$gunicorn_utilities_conf_setup" "$gunicorn_server_conf_setup"
+# sudo nano /etc/supervisor/conf.d/gunicorn.conf
+
+# step 8
+# Define the source and destination paths
+gunicorn_utilities_conf_setup="utilities/django/gunicorn.conf"
+gunicorn_server_conf_setup="/etc/supervisor/conf.d/gunicorn.conf"
+echo "Copying configuration files from utilities..."
+sudo cp "$gunicorn_utilities_conf_setup" "$gunicorn_server_conf_setup"
+# Create log directory for Gunicorn
+sudo mkdir -p /var/log/gunicorn
+nginx_utilities_conf_setup="utilities/django/nginx.conf"
+nginx_server_conf_setup="/etc/nginx/nginx.conf"
+echo "Updating user directive in nginx.conf to run as root..."
+sed -i "s/^user .*/user root;/" "$nginx_utilities_conf_setup"
+sudo cp "$nginx_utilities_conf_setup" "$nginx_server_conf_setup"
+
+django_utilities_conf_setup="utilities/django/django.conf"
+django_server_conf_setup="/etc/nginx/sites-available/django.conf"
+# Define the current and new IP addresses
+current_ip="35.165.93.124"
+new_ip="$(curl -s ifconfig.me)"  # Replace with the actual new IP address
+# Replace the IP address in the configuration file
+echo "Updating IP address in django.conf..."
+sed -i "s/$current_ip/$new_ip/g" "$django_utilities_conf_setup"
+
+sudo cp "$django_utilities_conf_setup" "$django_server_conf_setup"
+# Enable the Nginx site configuration
+sudo ln -s /etc/nginx/sites-available/django.conf /etc/nginx/sites-enabled
+sudo nginx -t
+# Restart Nginx
+sudo service nginx restart
+# Reload Supervisor configuration
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl status
+
